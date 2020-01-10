595c152dfafe43c5dd3ad7dbb5a40d98f8a93535
David Ricketts <david.ricketts119@gmail.com>
2020-01-10 16:15:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+
+
+[1, 2, 3, -9].map{|n| n * -1}
+
+
+dune.map {|n| n}
+
+
+map[1, 2, 3, -9]){|n| n * 2}
+
+
+map([1, 2, 3, -9]){|n| n * n}
+
+
+reduce(source_array){|memo, n| memo + n}
+
+
+reduce(source_array, starting_point){|memo, n| memo + n}
+
+
+reduce(source_array){|memo, n| memo && n}
+
+reduce(source_array){|memo, n| memo || n}
+
+
